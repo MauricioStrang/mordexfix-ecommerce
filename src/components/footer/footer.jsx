834ef8c6c9d034255e2cb521037ca1d2db4e8f46
MauricioStrang <strangmauricio@gmail.com>
2024-11-10 21:48:42 -0300

    html for footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -15,6 +15,48 @@ const Footer =()=> {
             <svg></svg>
             <svg></svg>
             <svg></svg>
+            <div>sign up for our mordexFix mailing list</div>
+        </div>
+
+        <div>
+          <div>
+            Email Adress
+          </div>
+          <div>
+            Sign Up
+          </div>
+
+          <div>
+            <h1>Your Account</h1>
+            <h1>FAQ</h1>
+            <h1>Contact Us</h1>
+            <h1>Blog</h1>
+            <h1>Search</h1>
+            <h1>Terms</h1>
+            <p>Copyright @ 2024 mordexFix LLC.</p>
+            <p>powered by your mom</p>
+          </div>
+
+          <div>
+            <div>
+              Argentina USD
+            </div>
+
+            <div>
+              <svg></svg>
+              <svg></svg>
+              <svg></svg>
+              <svg></svg>
+              <svg></svg>
+              <svg></svg>
+              <svg></svg>
+              <svg></svg>
+              <svg></svg>
+              <svg></svg>
+              <svg></svg>
+              <svg></svg>
+            </div>
+          </div>
         </div>
     </div>
   );
