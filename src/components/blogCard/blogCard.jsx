afcaca77ec69cc22a9533de50617d61cc4dd1916
MauricioStrang <strangmauricio@gmail.com>
2024-12-08 21:52:50 -0300

    blog card

--- src/components/blogCard/blogCard.jsx
+++ src/components/blogCard/blogCard.jsx
@@ -6,6 +6,10 @@ const BlogCard = ({blogImage})=>{
     return(
         <div className={styles.container}>
             <Image src={blogImage} width="500" height="250"/>
+            <div className={styles.date}></div>
+            <div className={styles.title}></div>
+            <div className={styles.text}></div>
+            <div className={styles.link}>Read now</div>
         </div>
     )
 };
