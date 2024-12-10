d73eeb3fce307886b493220edc02c72ee9b1b416
MauricioStrang <strangmauricio@gmail.com>
2024-12-09 21:18:45 -0300

    text test

--- src/components/blogCard/blogCard.jsx
+++ src/components/blogCard/blogCard.jsx
@@ -8,7 +8,9 @@ const BlogCard = ({blogImage})=>{
             <Image src={blogImage} width="500" height="250"/>
             <div className={styles.date}></div>
             <div className={styles.title}></div>
-            <div className={styles.text}></div>
+            <div className={styles.text}>
+                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum animi delectus debitis, consequatur, iusto suscipit laborum autem minima nemo temporibus, libero iste? Iste natus eum ratione provident deserunt odio laudantium?
+            </div>
             <div className={styles.link}>Read now</div>
         </div>
     )
