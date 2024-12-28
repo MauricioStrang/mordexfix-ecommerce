e42e83512f27f436cd47338e89471293d9896167
MauricioStrang <strangmauricio@gmail.com>
2024-12-28 20:08:35 -0300

    submissions page

---
+++ src/app/submissions/page.jsx
@@ -0,0 +1,11 @@
+import styles from "./submissions.module.css"
+
+const SubmissionsPage =()=> {
+  return (
+    <div className={styles.container}>
+        
+    </div>
+  );
+}
+
+export default SubmissionsPage;
