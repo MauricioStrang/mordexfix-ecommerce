5212b8729090cc7ddf9335d8ef1edc4035cabb9e
MauricioStrang <strangmauricio@gmail.com>
2024-12-29 15:03:36 -0300

    giveaway page

---
+++ src/app/giveaway/page.jsx
@@ -0,0 +1,11 @@
+import styles from './giveaway.module.css'
+
+const giveawayPage = ()=>{
+    return(
+        <div className={styles.container}>
+
+        </div>
+    )
+}
+
+export default giveawayPage;
